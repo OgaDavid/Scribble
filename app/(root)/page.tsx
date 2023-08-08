724d3dc62b3e32caf757b4c3312d306c5ccdcaf8
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-08-07 17:07:08 -1100

    fix(page.tsx): fix incorrect start sentence
    changes lets build scribble to lets build scribble right now

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Lets build Scribble now!
+      Lets build Scribble right now!
     </main>
   )
 }
