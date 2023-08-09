c297d6d83d41ab79a0db1bdcb0159b347220aaac
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-08-09 04:07:55 -1100

    fix(tooltip): wrong tooltip import

--- components/TooltipContainer.tsx
+++ components/TooltipContainer.tsx
@@ -3,7 +3,7 @@ import {
   TooltipContent,
   TooltipProvider,
   TooltipTrigger,
-} from "@/components/ui/tooltip";
+} from "@/components/ui/Tooltip";
 
 export const TooltipContainer = ({
   children,
