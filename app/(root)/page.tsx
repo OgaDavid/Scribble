2153faefde2a01ce687c046825336e6814df933a
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-08-19 02:02:54 -1100

    fix(folder): folder structure

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,9 @@
-import Companies from "@/components/Companies";
+import Companies from "@/components/Landing Page/Companies";
 import Container from "@/components/Container";
-import FAQ from "@/components/Faq/FAQ";
-import GetStarted from "@/components/GetStarted";
-import Hero from "@/components/Hero/Hero";
-import Publish from "@/components/Publish";
+import FAQ from "@/components/Landing Page/Faq/FAQ";
+import GetStarted from "@/components/Landing Page/GetStarted";
+import Hero from "@/components/Landing Page/Hero/Hero";
+import Publish from "@/components/Landing Page/Publish";
 
 export default function Home() {
   return (
