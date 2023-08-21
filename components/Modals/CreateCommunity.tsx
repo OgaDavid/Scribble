3faf948e513b8ff1320a326cfba2f49af7a892b8
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-08-21 11:50:24 -1100

    feat(community): create community modal content

--- components/Modals/CreateCommunity.tsx
+++ components/Modals/CreateCommunity.tsx
@@ -7,11 +7,16 @@ import { AlertCircle } from "lucide-react";
 import { useState } from "react";
 import { Label } from "@/components/ui/Label";
 import { cn } from "@/lib/utils";
+import { Separator } from "@/components/ui/separator";
+import { Checkbox } from "@/components/ui/checkbox";
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
+import { Button } from "../ui/Button";
 
 export const CreateCommunity = () => {
   const createCommunityModal = useCreateCommunityModal();
 
   const [communityName, setCommunityName] = useState("");
+  const [communityType, setCommunityType] = useState("public");
   const [charsRemaining, setCharsRemaining] = useState(21);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -19,19 +24,27 @@ export const CreateCommunity = () => {
     setCommunityName(event.target.value);
     setCharsRemaining(21 - event.target.value.length);
   };
+
+  const onCommunityTypeChange = (
+    event: React.ChangeEvent<HTMLButtonElement>
+  ) => {
+    setCommunityType(event.target.name);
+    console.log(event.target.name);
+  };
+
   return (
     <Modal
-      title="Create a Community"
-      description="Be an author! Create your community"
+      title="Create a Community🎉"
+      description="Be an author! Create your own community."
       isOpen={createCommunityModal.isOpen}
       onClose={createCommunityModal.onClose}
     >
-      <div>
+      <div className="flex flex-col">
         <div>
           <Label htmlFor="community name">Name</Label>
           <span className="text-red-600 py-1 font-medium flex items-center gap-1 text-xs">
-            <AlertCircle className="w-3 h-3" />
-            Community names including capitalization cannot be edited.
+            <AlertCircle className="w-3 h-3 max-md:hidden" />
+            Community names including capitalization cannot be edited!
           </span>
           <Input
             value={communityName}
@@ -48,8 +61,56 @@ export const CreateCommunity = () => {
             {charsRemaining} characters remaining.
           </p>
         </div>
+        <Separator className="my-3" />
         <div>
-
+          <h3 className="font-bold pb-3">Community Type</h3>
+          <RadioGroup defaultValue="public">
+            <div className="flex items-top space-x-2">
+              <RadioGroupItem onChange={onCommunityTypeChange} value="public" id="public" />
+              <div className="grid gap-1.5 leading-none">
+                <label
+                  htmlFor="public"
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  Public
+                </label>
+                <p className="text-xs text-muted-foreground">
+                  Anyone can view, post and comment to this community. 
+                </p>
+              </div>
+            </div>
+            <div className="flex items-top space-x-2">
+              <RadioGroupItem onChange={onCommunityTypeChange} value="private" id="private" />
+              <div className="grid gap-1.5 leading-none">
+                <label
+                  htmlFor="private"
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  Private
+                </label>
+                <p className="text-xs text-muted-foreground">
+                  Only approved users can view, post, and comment to this community.
+                </p>
+              </div>
+            </div>
+            <div className="flex items-top space-x-2">
+              <RadioGroupItem onChange={onCommunityTypeChange} value="restricted" id="restricted" />
+              <div className="grid gap-1.5 leading-none">
+                <label
+                  htmlFor="restricted"
+                  className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                >
+                  Restricted
+                </label>
+                <p className="text-xs text-muted-foreground">
+                  Any one can view this community but, only approved users can post.
+                </p>
+              </div>
+            </div>
+          </RadioGroup>
+        </div>
+        <div className="flex mt-5 md:justify-end">
+          <Button className="max-md:w-full">Create community</Button>
         </div>
       </div>
     </Modal>
---
+++ components/ui/radio-group.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import * as React from "react"
+import * as RadioGroupPrimitive from "@radix-ui/react-radio-group"
+import { Check } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+
+const RadioGroup = React.forwardRef<
+  React.ElementRef<typeof RadioGroupPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Root>
+>(({ className, ...props }, ref) => {
+  return (
+    <RadioGroupPrimitive.Root
+      className={cn("grid gap-2", className)}
+      {...props}
+      ref={ref}
+    />
+  )
+})
+RadioGroup.displayName = RadioGroupPrimitive.Root.displayName
+
+const RadioGroupItem = React.forwardRef<
+  React.ElementRef<typeof RadioGroupPrimitive.Item>,
+  React.ComponentPropsWithoutRef<typeof RadioGroupPrimitive.Item>
+>(({ className, children, ...props }, ref) => {
+  return (
+    <RadioGroupPrimitive.Item
+      ref={ref}
+      className={cn(
+        "peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
+        className
+      )}
+      {...props}
+    >
+      <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
+        <Check className="h-4 w-4" />
+      </RadioGroupPrimitive.Indicator>
+    </RadioGroupPrimitive.Item>
+  )
+})
+RadioGroupItem.displayName = RadioGroupPrimitive.Item.displayName
+
+export { RadioGroup, RadioGroupItem }
