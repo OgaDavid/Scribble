73ffaf1e4fe4e7e045fc6ba5a2a3c16b379ec788
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-08-21 13:14:56 -1100

    feat(community): create community functionality

--- components/Modals/CreateCommunity.tsx
+++ components/Modals/CreateCommunity.tsx
@@ -5,19 +5,26 @@ import { useState } from "react";
 import { Modal } from "@/components/ui/Modal";
 import { useCreateCommunityModal } from "@/hooks/use-create-community-modal";
 import { Input } from "@/components/ui/Input";
-import { AlertCircle } from "lucide-react";
+import { AlertCircle, Loader2 } from "lucide-react";
 import { Label } from "@/components/ui/Label";
 import { cn } from "@/lib/utils";
 import { Separator } from "@/components/ui/separator";
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
 import { Button } from "../ui/Button";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { auth, firestore } from "@/lib/firebase/firebase.config";
+import { useAuthState } from "react-firebase-hooks/auth";
 
 export const CreateCommunity = () => {
+  const [user] = useAuthState(auth);
+
   const createCommunityModal = useCreateCommunityModal();
 
   const [communityName, setCommunityName] = useState("");
   const [communityType, setCommunityType] = useState("public");
   const [charsRemaining, setCharsRemaining] = useState(21);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.value.length > 21) return;
@@ -29,7 +36,43 @@ export const CreateCommunity = () => {
     event: React.ChangeEvent<HTMLButtonElement>
   ) => {
     setCommunityType(event.target.name);
-    console.log(event.target.name);
+    console.log(communityType);
+  };
+
+  const handleCreateCommunity = async () => {
+    if (error) {
+      setError("");
+    }
+    const format = /[ `!@#$%^&*()+\-=\[\]{};':"\\|,.<>\/?~]/;
+    if (format.test(communityName) || communityName.length < 3) {
+      setError(
+        "Community names must be between 3-21 characters, and can only contain letters, numbers and underscores (_) with no spaces."
+      );
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const communityDocRef = doc(firestore, "communities", communityName);
+      const communityDoc = await getDoc(communityDocRef);
+
+      if (communityDoc.exists()) {
+        throw new Error(`Sorry, "${communityName}" is taken. Try another community name.`);
+      }
+
+      await setDoc(communityDocRef, {
+        creatorID: user?.uid,
+        createdAt: serverTimestamp(),
+        numberOfMembers: 1,
+        privacyType: communityType,
+      });
+    } catch (error: any) {
+      console.log("Handle create community error", error);
+      setError(error.message);
+    }
+
+    setLoading(false);
   };
 
   return (
@@ -60,13 +103,18 @@ export const CreateCommunity = () => {
           >
             {charsRemaining} characters remaining.
           </p>
+          {error && <p className="text-xs py-1 pl-1 text-red-600">{error}</p>}
         </div>
         <Separator className="my-3" />
         <div>
           <h3 className="font-bold pb-3">Community Type</h3>
           <RadioGroup defaultValue="public">
-            <div className="flex items-top space-x-2">
-              <RadioGroupItem onChange={onCommunityTypeChange} value="public" id="public" />
+            <div onClick={() => setCommunityType("public")} className="flex items-top space-x-2">
+              <RadioGroupItem
+                // onChange={(onCommunityTypeChange)}
+                value="public"
+                id="public"
+              />
               <div className="grid gap-1.5 leading-none">
                 <label
                   htmlFor="public"
@@ -75,12 +123,16 @@ export const CreateCommunity = () => {
                   Public
                 </label>
                 <p className="text-xs text-muted-foreground">
-                  Anyone can view, post and comment to this community. 
+                  Anyone can view, post and comment to this community.
                 </p>
               </div>
             </div>
-            <div className="flex items-top space-x-2">
-              <RadioGroupItem onChange={onCommunityTypeChange} value="private" id="private" />
+            <div onClick={() => setCommunityType("private")} className="flex items-top space-x-2">
+              <RadioGroupItem
+                // onChange={(onCommunityTypeChange)}
+                value="private"
+                id="private"
+              />
               <div className="grid gap-1.5 leading-none">
                 <label
                   htmlFor="private"
@@ -89,12 +141,17 @@ export const CreateCommunity = () => {
                   Private
                 </label>
                 <p className="text-xs text-muted-foreground">
-                  Only approved users can view, post, and comment to this community.
+                  Only approved users can view, post, and comment to this
+                  community.
                 </p>
               </div>
             </div>
-            <div className="flex items-top space-x-2">
-              <RadioGroupItem onChange={onCommunityTypeChange} value="restricted" id="restricted" />
+            <div onClick={() => setCommunityType("restricted")} className="flex items-top space-x-2">
+              <RadioGroupItem
+                // onChange={(onCommunityTypeChange)}
+                value="restricted"
+                id="restricted"
+              />
               <div className="grid gap-1.5 leading-none">
                 <label
                   htmlFor="restricted"
@@ -103,14 +160,22 @@ export const CreateCommunity = () => {
                   Restricted
                 </label>
                 <p className="text-xs text-muted-foreground">
-                  Any one can view this community but, only approved users can post.
+                  Any one can view this community but, only approved users can
+                  post.
                 </p>
               </div>
             </div>
           </RadioGroup>
         </div>
         <div className="flex mt-5 md:justify-end">
-          <Button className="max-md:w-full">Create community</Button>
+          <Button onClick={handleCreateCommunity} className="max-md:w-full">
+            <div className="flex items-center gap-1">
+              Create community{" "}
+              {loading && (
+                <Loader2 className="w-4 text-white h-4 animate-spin" />
+              )}
+            </div>
+          </Button>
         </div>
       </div>
     </Modal>
