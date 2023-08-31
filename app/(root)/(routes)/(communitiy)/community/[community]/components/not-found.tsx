ded255c87e39e137ffbea92e25838e87f593a70b
OgaDavid <oluwademiladedavidoga@gmail.com>
2023-08-31 01:27:28 -1100

    feat(create community): not found page

---
+++ app/(root)/(routes)/(communitiy)/community/[community]/components/not-found.tsx
@@ -0,0 +1,44 @@
+"use client"
+
+import Container from "@/components/Container";
+import { TooltipContainer } from "@/components/TooltipContainer";
+import { useCreateCommunityModal } from "@/hooks/use-create-community-modal";
+import Image from "next/image";
+import React from "react";
+
+const NotFound = ({ name }: { name: string }) => {
+  const onOpen = useCreateCommunityModal((state) => state.onOpen);
+  return (
+    <Container>
+      <div className="py-16 md:py-20 text-2xl">
+        <div className="flex flex-col md:selection:pt-36 items-center justify-center">
+          <h1 className="text-4xl md:text-5xl max- text-center max-w-[800px] pt-5 md:leading-[62px] font-extrabold">
+            Uh oh!{" "}
+          </h1>
+          <p className="text-brand-gray text-sm md:text-lg text-center max-w-[800px] pt-3">
+            We've searched for {name}, but we just can&apos;t find it. Be the
+            first to
+            <TooltipContainer text={`Click here to create ${name}.`}>
+              <span
+                onClick={onOpen}
+                className="underline italic cursor-pointer"
+              >
+                {" "}
+                create this community.
+              </span>
+            </TooltipContainer>
+          </p>
+          <Image
+          className="mt-10"
+            src="/assets/images/not-found.png"
+            alt="404"
+            width={645}
+            height={428}
+          />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default NotFound;
--- app/(root)/(routes)/(communitiy)/community/[community]/page.tsx
+++ app/(root)/(routes)/(communitiy)/community/[community]/page.tsx
@@ -1,17 +1,22 @@
 import { getCommunity } from "@/actions/get-community";
-import React from "react";
-
+import NotFound from "./components/not-found";
+import Container from "@/components/Container";
 const CommunityPage = async ({ params }: { params: { community: string } }) => {
   const communityData = await getCommunity(params.community);
 
-  console.log(communityData)
+  console.log(communityData);
 
   if (communityData === null) {
-
-    return <div className="py-20 text-2xl">not found</div>
+    return <NotFound name={params.community} />;
   }
 
-  return <div className="py-20 text-2xl">welcome to {communityData.id}</div>;
+  return (
+    <>
+      <Container>
+        <div className="py-16 md:py-20 text-2xl">welcome to {communityData.id}</div>
+      </Container>
+    </>
+  );
 };
 
 export default CommunityPage;
